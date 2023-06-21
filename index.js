5543b1b34b81faa29524ce537b21b864c80b5711
lucaspham098 <lucaspham098@gmail.com>
2023-06-20 20:08:51 -0400

    fix bug so full array of players sent to client

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express()
 require('dotenv').config()
 const CLIENT_URL = process.env.CLIENT_URL
 const PORT = process.env.PORT
+
 const io = require("socket.io")(3001, {
     cors: {
-        origin: ['https://admin.socket.io/', CLIENT_URL]
+        origin: ["https://admin.socket.io/", CLIENT_URL]
     }
 })
 
@@ -20,22 +21,56 @@ io.on('connection', socket => {
     console.log(`Here : ${socket.id}`)
 
     socket.on('createRoom', (room) => {
-        socket.join(room)
-        rooms[room] = {
-            participants: [socket.id],
+        if (!rooms[room]) {
+            socket.join(room)
+            rooms[room] = {
+                participants: [socket.id],
+            }
+            console.log(`${socket.id} created room ${room}`);
+            socket.emit('joinedRoom')
+        } else {
+            socket.emit('joinCreatedRoom')
         }
-        console.log(`${socket.id} created room ${room} with ${rooms[room].participants.length} participant(s)`);
+
     })
 
     socket.on('joinRoom', room => {
+        socket.join(room)
+        rooms[room].participants.push(socket.id);
+        console.log(`${socket.id} joined room ${room} with ${rooms[room].participants.length}`)
+        console.log(`${rooms[room].participants}`)
+        socket.emit('joinedRoom')
+    })
+
+    socket.on('findRoom', room => {
         if (rooms[room] && rooms[room].participants.length < 8) {
-            socket.join(room);
-            rooms[room].participants.push(socket.id);
-            console.log(`${socket.id} joined room ${room} with ${rooms[room].participants.length}`);
+            socket.emit('navToLobby')
         } else {
             const message = 'Room does not exist or is full'
             socket.emit('errorMessage', message);
         }
+    });
+
+    socket.on('askForPlayers', room => {
+        console.log('recieved askplayers')
+        if (rooms[room]) {
+            const players = rooms[room].participants.map((player) => {
+                return player
+            })
+            io.in(room).emit('players', players)
+        }
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} left`)
+        Object.keys(rooms).forEach((room) => {
+            const participants = rooms[room].participants;
+            const participantIndex = participants.indexOf(socket.id);
+            if (participantIndex !== -1) {
+                participants.splice(participantIndex, 1);
+                console.log(`${socket.id} left room ${room}`);
+            }
+        });
     });
 })
 
